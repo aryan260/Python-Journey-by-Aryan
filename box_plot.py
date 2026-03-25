5a6a37e24bd27d19acdf8a1aa0f75cbc2cd9f91c
ARYAN SINGH <thakuraryansingh11825114@gmail.com>
2026-03-25 22:34:48 +0530

    Enhance box plot with new data and styling options
    Modified box_plot.py to visualize two datasets with enhanced styling and mean indicators.

--- box_plot.py
+++ box_plot.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
-x=[1, 2, 3, 4, 5]
-y=[1,2, 3, 4, 5]
 
-plt.boxplot(x,y)
+x=[10,20,30,40,50,60,70]
+x1=[10,40,90,16,36,49]
+
+y=[x,x1]
+
+plt.boxplot(y,labels=["Python","c++"],showmeans=True,sym=" ",boxprops=dict(color="r"),capprops=dict(color="r"),whiskerprops=dict(color="g"))
+
+#plt.boxplot(x1,labels=["Python"],showmeans=True,sym=" ",boxprops=dict(color="r"),capprops=dict(color="r"),whiskerprops=dict(color="g"))
+
 plt.show()
